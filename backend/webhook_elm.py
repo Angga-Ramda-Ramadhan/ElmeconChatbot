4cf539ada0599f5b5a0ebe95696030b99b9bbecb
Angga-Ramda-Ramadhan <anggaramdawork@gmail.com>
2026-04-28 11:09:11 +0700

    update webhook

---
+++ backend/webhook_elm.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+@app.route('/webhook', methods=['POST', 'GET'])
+def webhook_elm():
+
+    if request.method == 'POST':
+        data = request.json
+
+        print('Received POST data: ', data)
+
+        return jsonify({
+            'message':'Berhasil terima post webhook'
+        })
+    
+    elif request.method == 'GET':
+        data = request.json
+        print('Received Get Data: ', data)
+        return jsonify({
+            'message':'Berhasil terima get webhook'
+        })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
